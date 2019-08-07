a8a5a2e1ede7d22e6789614dffce95208e762b10
Michael Ferber <mferber8@gmail.com>
2019-08-07 09:38:16 -0500

    fixed encoding issue caused by jupyter notebook

--- pybank/main.py
+++ pybank/main.py
@@ -1,22 +1,22 @@
 from pathlib import Path
 import csv
 import numpy as np
-​
+
 csvpath = ('Resources/budget_data.csv')
-=
-#initialize variables
-​
-profits = []
+
+#initialize variable profits = []
+profits=[]
 #use csvreader to open file
 with open (csvpath, 'r') as csvfile:
     
     #pass opened file into csv reader
     
     csvreader = csv.reader(csvfile, delimiter = ',')
-​
+
 #skip header and get profit values into profits list
+
     header = next(csvreader)
-    
+
     for row in csvreader:
         
         profit = int(row[1])
@@ -24,20 +24,19 @@ with open (csvpath, 'r') as csvfile:
         profits.append(profit)
             
 # initialize metric variables then loop by rows
-​
 min_profit = 0
 max_profit = 0
 total_profit = 0
 months_count = 0
 min_profit_date = ""
 average_delta_profit = np.mean(np.diff(profits))
-​
+
 for profit in profits:
-​
+    
     months_count += 1
-​
+
     total_profit += profit
-​
+
     if min_profit == 0:
         min_profit = profit
         max_profit = profit
@@ -50,17 +49,9 @@ for profit in profits:
 #Output to text file
 print("Financial Analysis")               
 print("-------------------")
-metrics = []
 print(f"Total Months: {months_count}")
 print(f"Total Profit: ${total_profit}")
 print(f"Average Change: ${int(average_delta_profit)}")
 print(f"Greatest Increase in Profits: ${max_profit}")
 print(f"Greatest Decrease in Profits: {min_profit_date} ${min_profit}")
-Financial Analysis
--------------------
-Total Months: 86
-Total Profit: $38382578
-Average Change: $-2315
-Greatest Increase in Profits: $1170593
-Greatest Decrease in Profits:  $-1196225
 
