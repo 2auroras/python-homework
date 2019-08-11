47b50cc77aaed4ebf8a7046e813389f991e1cea6
Michael Ferber <mferber8@gmail.com>
2019-08-11 14:26:48 -0500

    Added comments to main.py

--- pyramen/main.py
+++ pyramen/main.py
@@ -1,3 +1,6 @@
+## After many hours I consulted with a classmate to get what is seen below.  
+# I am still having trouble accessing values of a key in a nested dictionary
+
 from pathlib import Path
 import csv
 
@@ -36,27 +39,31 @@ with open (csvsales, 'r') as csv_sales_file:
     
     header = next(csv_sales_file)
     
+    #only appended items to be used
     for item in csvsalesreader:
         sales_items.append([item[4],item[3]])
-        #sales_quantity.append(item[3])
+        
 
 
 ## Report Dictionary
 report={}
 
-
+##Iterated menu and create metrics dictionary
 for menu_item in menu:
+    #set each items quantity to 0 - this needs to be inside loop so it is reset for each item
     quantity = int(0)
+##Iterated sales csv and used += to add up sales per item by name
     for sale in sales_items:
         if menu_item[0] == sale[0]:
             quantity += int(sale[1])
-
+##Took numbers from menu and combined with quantity total gotten above
     item_metrics = {'Price' :float(menu_item[1])},{'COGS' : float(menu_item[2])},{'Gross' : float(menu_item[1])-float(menu_item[2])},{'Number Sold' : quantity},{'Total Profit' : (quantity)*(float(menu_item[1])-float(menu_item[2]))} 
 
     key = menu_item[0]
 
     report[key] = item_metrics
-
+##Wanted to filter out the menu items that have no sales, but am having trouble understanding how to access "Number Sold" value
+##Print all items and their profit
 for key,item_metrics in report.items():
     print(f"{key}\n{item_metrics}")
 
